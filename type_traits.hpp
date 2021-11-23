0b2df75f50b49eb7ccf5e11183993b0b237f0e2f
AtheDev <athe.dev@gmail.com>
2021-11-23 17:22:14 +0100

    Ajout de la partie Const dans is_integral

--- type_traits.hpp
+++ type_traits.hpp
@@ -70,16 +70,16 @@ namespace   ft
     struct  is_integral<signed char> : public true_type {};
 
     template<>
-    struct  is_integral<short> : public true_type {};
+    struct  is_integral<short int> : public true_type {};
 
     template<>
     struct  is_integral<int> : public true_type {};
 
     template<>
-    struct  is_integral<long> : public true_type {};
+    struct  is_integral<long int> : public true_type {};
 
     template<>
-    struct  is_integral<long long> : public true_type {};
+    struct  is_integral<long long int> : public true_type {};
 
     template<>
     struct  is_integral<unsigned char> : public true_type {};
@@ -88,13 +88,54 @@ namespace   ft
     struct  is_integral<unsigned int> : public true_type {};
 
     template<>
-    struct  is_integral<unsigned long> : public true_type {};
+    struct  is_integral<unsigned long int> : public true_type {};
 
     template<>
-    struct  is_integral<unsigned long long> : public true_type {};
+    struct  is_integral<unsigned long long int> : public true_type {};
 
     template<>
-    struct  is_integral<unsigned short> : public true_type {};
+    struct  is_integral<unsigned short int> : public true_type {};
+
+
+
+    template<>
+    struct  is_integral<const bool> : public true_type {};
+
+    template<>
+    struct  is_integral<const char> : public true_type {};
+
+    template<>
+    struct  is_integral<const wchar_t> : public true_type {};
+
+    template<>
+    struct  is_integral<const signed char> : public true_type {};
+
+    template<>
+    struct  is_integral<const short int> : public true_type {};
+
+    template<>
+    struct  is_integral<const int> : public true_type {};
+
+    template<>
+    struct  is_integral<const long int> : public true_type {};
+
+    template<>
+    struct  is_integral<const long long int> : public true_type {};
+
+    template<>
+    struct  is_integral<const unsigned char> : public true_type {};
+
+    template<>
+    struct  is_integral<const unsigned int> : public true_type {};
+
+    template<>
+    struct  is_integral<const unsigned long int> : public true_type {};
+
+    template<>
+    struct  is_integral<const unsigned long long int> : public true_type {};
+
+    template<>
+    struct  is_integral<const unsigned short int> : public true_type {};
 
 
     /* ********************************* */
