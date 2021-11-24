f8b095545b270bc5e24e12050f72ce600a43e275
AtheDev <athe.dev@gmail.com>
2021-11-24 23:42:38 +0100

    Mise en place des tests

--- main.cpp
+++ main.cpp
@@ -5,25 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adupuy <adupuy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/11 18:15:02 by adupuy            #+#    #+#             */
-/*   Updated: 2021/10/11 18:15:03 by adupuy           ###   ########.fr       */
+/*   Created: 2021/11/24 15:30:18 by adupuy            #+#    #+#             */
+/*   Updated: 2021/11/24 15:30:20 by adupuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vector.hpp"
-#include <vector>
-#include <iostream>
-#include "algorithm.hpp"
-#include "type_traits.hpp"
-#include "iterator.hpp"
-//#include <algorithm>
-
-
-void    testAlgorithm(void);
+#include "main.hpp"
 
 int main()
 {
-    //testAlgorithm();
-
-    return (0);
+    testVector();
+    return 0;
 }
